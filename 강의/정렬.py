29d129ffba6bc95cd66f693064b76011cda743d6
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-09-15 16:22:46 +0900

    강의3일차

---
+++ 강의/정렬.py
@@ -0,0 +1,11 @@
+array = list(map(int, input().split()))
+array.sort()
+print(array)
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i+1,len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+print(array)
