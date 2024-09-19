864cb722d7f562a2277a1c9dc92ad16a5c0a72cf
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-09-19 12:13:47 +0900

    강의 5주차

---
+++ 강의/람다.py
@@ -0,0 +1,9 @@
+import random
+
+max_a = lambda a, b: a if a > b else b
+
+printVar = lambda a: print(a)
+
+square = lambda a: a**2
+
+roll = lambda a: random.randint(1, a)
---
+++ 강의/삽입정렬.py
@@ -0,0 +1,12 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+for i in range(1, n):
+    key = array[i]
+    j = i - 1
+    while j >= 0 and array[j] > key:
+        array[j + 1] = array[j]
+        j -= 1
+    array[j + 1] = key
+
+print(array)
